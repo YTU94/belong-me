7d689a43048109d1e9d29eac8f289b9bdea54e1e
linyi <linyi@tsign.cn>
2019-09-04 11:17:35 +0800

    feat: 添加打包后文件

---
+++ build/precache-manifest.20e88313bf8f3ae79c5615cfde40ac9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ae5d4c6e8cd58346d5f2845ed010b62",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83033d7b00ef8375dde6",
+    "url": "/static/css/2.176bc974.chunk.css"
+  },
+  {
+    "revision": "43c5f7856e713e77feac",
+    "url": "/static/css/main.d4e39de7.chunk.css"
+  },
+  {
+    "revision": "83033d7b00ef8375dde6",
+    "url": "/static/js/2.047dd337.chunk.js"
+  },
+  {
+    "revision": "43c5f7856e713e77feac",
+    "url": "/static/js/main.10a22fa8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
