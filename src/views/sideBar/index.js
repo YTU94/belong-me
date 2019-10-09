6ddd26ab0192f388dd9cd0c65b8f985a406058ad
linyi <linyi@tsign.cn>
2019-10-09 14:47:06 +0800

    feat: 添加sideBar

---
+++ src/views/sideBar/index.js
@@ -0,0 +1,17 @@
+import React from "react"
+import "./index.less"
+import { Avatar } from "antd"
+
+function Index(params) {
+    return (
+        <div className='side-bar'>
+            <h2 className='text-align-center'>
+                林逸 —— <Avatar style={{ backgroundColor: '#87d068' }}>YTU</Avatar>
+            </h2>
+
+            <br />
+            <br />
+        </div>
+    )
+}
+export default Index
