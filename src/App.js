a780cdbecfd146aff576bbee5d1cb304196d696b
linyi <linyi@tsign.cn>
2019-12-06 16:35:47 +0800

    feat: 整理代码，增加新firame模块

--- src/App.js
+++ src/App.js
@@ -2,18 +2,37 @@ import React from "react"
 import "./App.less"
 import { HashRouter as Router, Route, Switch, Link, withRouter } from "react-router-dom"
 import Blog from "./views/blog"
-
-import Footer from "./components/footer"
-import Header from "./components/header"
+import F2e from "./views/f2e"
 import Home from "./views/home"
+import Footer from "./components/footer"
+
+const list = [
+    { path: "/", component: Home, name: "首页" },
+    { path: "/blog", component: Blog, name: "博客" },
+    { path: "/f2e", component: F2e, name: "前端之道" }
+]
 
 const Homes = withRouter(props => {
     return (
         <div className='root-app'>
-            <Header></Header>
+            <header className='header'>
+                <div className='bottom-bar'>
+                    <div className='left'>YTU-94</div>
+                    <div className='right'>
+                        {list.map(e => {
+                            return (
+                                <Link key={e.name} to={e.path}>
+                                    {e.name}
+                                </Link>
+                            )
+                        })}
+                    </div>
+                </div>
+            </header>
 
             <Switch>
                 <Route path='/blog' component={Blog} />
+                <Route path='/f2e' component={F2e} />
                 <Route path='/' component={Home} />
             </Switch>
 
---
+++ src/views/f2e/index.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+import "./index.less"
+
+function Index() {
+    return <iframe src='http://f2e.ytuj.cn/' frameborder='0' className='iframe-page'></iframe>
+}
+
+export default Index
