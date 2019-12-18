45a4ed319fbdbfc0ae6cc7f9b96d5532109ec296
linyi <linyi@tsign.cn>
2019-12-18 16:16:58 +0800

    feat: 添加备案号

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -3,7 +3,6 @@ import "./index.less"
 import { Button } from "antd"
 
 export default function Index(params) {
-
     return (
         <footer className='footer'>
             <h3>The story has not yet begun</h3>
@@ -17,12 +16,16 @@ export default function Index(params) {
             </div>
             <div className='bottom-bar'>
                 <div className='left'>Copyright © 2019 WebYtu. All rights reserved.</div>
+
                 <div className='right'>
                     <Button type='link' className='bt' href='https://github.com/YTU94/belong-me'>
                         github
                     </Button>
                 </div>
             </div>
+            <Button type='link' className='beian-bt bt' href='http://www.beian.miit.gov.cn'>
+                浙ICP备18054191号
+            </Button>
         </footer>
     )
 }
